00a4b5eef72b9fe61b953236501d404cb59021c4
Sean OToole <sean.otoole@seanotoole.net>
2022-04-10 12:11:48 -0500

    added EmptyForm for follow and unfollow functions. added test file

--- app/forms.py
+++ app/forms.py
@@ -40,3 +40,6 @@ class EditProfileForm(FlaskForm):
       user = User.query.filter_by(username=self.username.data).first()
       if user is not None:
         raise ValidationError('Please choose a different username')
+
+class EmptyForm(FlaskForm):
+  submit = SubmitField('Submit')
